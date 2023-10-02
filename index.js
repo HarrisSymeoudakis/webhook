5d3cb33f6f7058d3141df0710ea3a37027ecaa48
Charalampos Symeoudakis <CSymeoudakis@mspos.net>
2023-10-02 12:11:53 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
+const axios = require('axios'); // Import the axios library
 const app = express();
+const port = process.env.PORT || 10000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.post('/webhook', (req, res) => {
-    console.log('Received Webhook:', req.body);
-    res.status(200).send('OK');
+app.post('/webhook', async (req, res) => {
+  // Raw notification data is available in req.body
+  const notificationData = req.body;
+
+  // Access the nested values
+  const customerId = req.body.Body.Key.Details.customerid;
+  const action = req.body.Body.Key.Details.action;
+
+  // Log the values
+  console.log('Received Webhook - Customer ID:', customerId);
+  console.log('Received Webhook - Action:', action);
+
+  // Forward the data to your Pipedream request bin
+  try {
+    const response = await axios.post('https://eomrm4f2rn42efc.m.pipedream.net', {
+      notificationData: notificationData,
+    });
+
+    console.log('Forwarded data to Pipedream:', response.data);
+    res.status(200).send('Notification received and forwarded successfully.');
+  } catch (error) {
+    console.error('Error forwarding data to Pipedream:', error.message);
+    res.status(500).send('Error forwarding data to Pipedream');
+  }
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Webhook receiver listening on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
